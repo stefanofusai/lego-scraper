08daccc6beedc776f64a5becaa252a8391605b50
Stefano Fusai <stefanofusai@gmail.com>
2024-01-08 23:33:43 +0100

    Implement Disney

--- scraper/spiders/vendiloshop.py
+++ scraper/spiders/vendiloshop.py
@@ -1,100 +1,48 @@
-from scrapy.http import FormRequest
-
 from scraper.items import Item
 from scraper.spiders.base import BaseSpider
 
 
-class VendiloShopSpider(BaseSpider):
-    name = "vendiloshop"
+class DisneySpider(BaseSpider):
+    name = "disney"
 
-    allowed_domains = ["sniperfast.com"]
+    allowed_domains = ["shopdisney.it"]
     start_urls = [
-        "40539",
-        "40615",
-        "40623",
-        "40624",
-        "40625",
-        "40626",
-        "75290",
-        "75300",
-        "75301",
-        "75304",
-        "75312",
-        "75320",
-        "75323",
-        "75324",
-        "75327",
-        "75329",
-        "75330",
-        "75333",
-        "75336",
-        "75342",
-        "75345",
-        "75347",
-        "75348",
-        "75350",
-        "75351",
-        "75352",
-        "75353",
-        "75354",
-        "75356",
-        "75359",
-        "75360",
-        "75361",
-        "75364",
-        "75369",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461031848939",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461031897364",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461031897449",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461031935066",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032361154",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032775494",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461033557662",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032775562",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032775647",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032998572",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032998657",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461033557907",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461033557822",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461032998817",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461034028246",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461034028659",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=417167274937",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461034183822",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=461034183907",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=417167275439",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=417167275279",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=417167459662",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=417167275507",
+        "https://www.shopdisney.it/on/demandware.store/Sites-shopDisneyIT-Site/it_IT/Product-Variation?pid=417167454049",
     ]
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield FormRequest(
-                "https://api.sniperfast.com/search",
-                formdata={
-                    "key": "26d868-4d8dda-4d40ea-d98082-9a6c33",
-                    "name": "index_vndshp",
-                    "sort": "rel",
-                    "sort_num": "100",
-                    "sort_page": "1",
-                    "input[user_input]": url,
-                    "input[cat1]": "",
-                    "input[cat2]": "",
-                    "input[cat3]": "",
-                    "input[cat4]": "",
-                    "filters[manufacturer]": "",
-                    "filters[manufacturer_count]": "10",
-                    "filters[price][min]": "0",
-                    "filters[price][max]": "0",
-                    "filters[price][low]": "",
-                    "filters[price][high]": "",
-                    "last_change": "",
-                    "customer": "0_1",
-                    "apiv": "2",
-                    "sniper_session": "t9s67nf8jnrdtg8bobt2hjud6s",
-                },
-                callback=self.parse,
-                cb_kwargs={"url": url},
-            )
-
-    def parse(self, response, url):
-        try:
-            for result in response.json()["result"]:
-                if url in result["name"][0]:
-                    break
-
-            else:
-                return
-
-        except KeyError:
-            return
-
+    def parse(self, response):
+        result = response.json()["product"]
         yield Item(
-            site="Vendilo Shop",
+            site="Disney",
             id=result["id"],
-            url=result["url"],
-            image=result["img"],
-            title=result["name"][0],
+            url=f"https://www.shopdisney.it{result['selectedProductUrl']}",
+            image=result["images"]["large"][0]["url"],
+            title=result["productName"],
             currency="€",
-            price=float(result["price"].replace(",", ".")),
+            price=result["price"]["selling"]["value"],
             condition=None,
-            in_stock=result["quantity"] > 0,
+            in_stock=result["available"],
         )
